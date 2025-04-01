f813995145c921cf91928cda5166067bb62c802d
PaulScheer <paul.scheer@rl-institut.de>
2025-04-01 18:04:35 +0200

    Fix diagnostics hover

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -2,5 +2,17 @@
 -- Folding by indent is nice, without ignoring anything
 print 'Setting Foldmethods in nvim/myinit.lua'
 vim.opt.foldmethod = 'indent'
+vim.opt.foldlevel = 5
 --vim.opt.foldnestmax = 99
-vim.opt.foldignore = ''
+-- vim.opt.foldignore = ''
+
+vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { desc = 'Line Diagnostics' })
+vim.opt.colorcolumn = '100'
+
+vim.diagnostic.config {
+  virtual_text = false,
+  underline = true,
+  signs = true,
+}
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next Diagnostic' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Prev Diagnostic' })
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,4 +1,5 @@
 --   You can add your own plugins here or in other files in this directory!
+--   z
 --  I promise not to create any merge conflicts in this directory .)
 --
 -- See the kickstart.nvim README for more information
@@ -16,22 +17,27 @@ local harpoon = {
 
     vim.keymap.set('n', '<C-e>', function()
       harpoon_ui.toggle_quick_menu()
-    end)
+    end, { desc = 'Toogle quick menu in Harpoon' })
 
     vim.keymap.set('n', '<C-h>', function()
       harpoon_ui.nav_file(1)
-    end)
+    end, { desc = 'Go to file 1 in Harpoon' })
+
     vim.keymap.set('n', '<C-t>', function()
       harpoon_ui.nav_file(2)
-    end)
-    vim.keymap.set('n', '<C-n>', function()
-      harpoon_ui.nav_file(3)
-    end)
+    end, { desc = 'Go to file 2 in Harpoon' })
+
     vim.keymap.set('n', '<C-s>', function()
-      harpoon_ui.nav_file(4)
-    end)
+      harpoon_ui.nav_file(3)
+    end, { desc = 'Go to file 3 in Harpoon' })
+
+    vim.keymap.set('n', '<C-n>', function()
+      harpoon_ui.nav_next()
+    end, { desc = 'Go to next Harpoon File' })
   end,
 }
 return {
   harpoon,
+  { 'tpope/vim-fugitive' },
+  { 'echasnovski/mini.surround' },
 }
