9c63a2a77cf47bbc38f9a57a6756bfb7f1601e71
paul.scheer <Paul.Scheer@rl-institut.de>
2025-04-04 13:25:48 +0200

    Comment key maps for mini.move

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -40,7 +40,26 @@ return {
   harpoon,
   { 'tpope/vim-fugitive' },
   { 'echasnovski/mini.surround' },
-  { 'echasnovski/mini.move', config = require('mini.move').setup() },
+  {
+    'echasnovski/mini.move',
+    config = require('mini.move').setup(),
+    -- default mappings set by setup. Added as comment for documentation
+    --[[
+    mappings = {
+    -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+    left = '<M-h>',
+    right = '<M-l>',
+    down = '<M-j>',
+    up = '<M-k>',
+
+    -- Move current line in Normal mode
+    line_left = '<M-h>',
+    line_right = '<M-l>',
+    line_down = '<M-j>',
+    line_up = '<M-k>',
+    },
+--]]
+  },
   {
     'echasnovski/mini.sessions',
     config = function()
