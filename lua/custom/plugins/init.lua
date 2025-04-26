f40c20bc4d74d61bf218f894185f89d102ead8cb
PaulScheerRLI <paul.scheer@rl-institut.de>
2025-04-26 16:37:12 +0200

    Remove hotkey from harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -21,10 +21,10 @@ local harpoon = {
     vim.keymap.set('n', '<C-s>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
-
-    vim.keymap.set('n', '<C-h>', function()
-      harpoon:list():select(1)
-    end)
+    -- destroys fast move between windows
+    -- vim.keymap.set('n', '<C-h>', function()
+    -- harpoon:list():select(1)
+    -- end)
     vim.keymap.set('n', '<C-t>', function()
       harpoon:list():select(2)
     end)
