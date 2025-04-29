027847cd3781b5f7e4833336221a13a2bb296570
PaulScheer <paul.scheer@rl-institut.de>
2025-04-29 10:26:35 +0200

    Make Conform formatter more verbose

--- init.lua
+++ init.lua
@@ -747,7 +747,8 @@ require('lazy').setup({
       },
     },
     opts = {
-      notify_on_error = false,
+      notifiy_no_formatter = true,
+      notify_on_error = true,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
