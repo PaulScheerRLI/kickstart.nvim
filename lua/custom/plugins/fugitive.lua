7c1fffb50d3b26ec72626be1d459dffb617a1be0
PaulScheer <paul.scheer@rl-institut.de>
2025-05-12 11:13:21 +0200

    Make message more descriptive

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -2,6 +2,6 @@
 vim.keymap.set('n', '<leader>ag', function()
   local file = vim.fn.expand '%'
   vim.cmd 'Gwrite'
-  print('File added: ' .. file)
+  print('File added to git: ' .. file)
 end, { desc = '<leader> [A]dd file to [g]it' })
 return { 'tpope/vim-fugitive' }
