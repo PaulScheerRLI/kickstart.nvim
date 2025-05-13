62718e2e052d99ff729490f2d438e209da7df239
PaulScheer <paul.scheer@rl-institut.de>
2025-05-13 10:35:08 +0200

    Add window number to lualine

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -69,6 +69,9 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     config = function()
+      local function window()
+        return vim.api.nvim_win_get_number(0)
+      end
       require('lualine').setup {
         --sections = {
         --          lualine_c = {
@@ -86,6 +89,42 @@ return {
         --          },
         --},
         -- OR in winbar
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          always_show_tabline = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 100,
+            tabline = 100,
+            winbar = 100,
+          },
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = { window },
+        },
+        tabline = {},
         winbar = {
           lualine_c = {
             'navic',
@@ -93,6 +132,8 @@ return {
             navic_opts = nil,
           },
         },
+        inactive_winbar = {},
+        extensions = {},
       }
     end,
   },
