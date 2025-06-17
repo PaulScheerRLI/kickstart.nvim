7a699b93a88e8bcb247787eab11612bfbc472435
PaulScheer <paul.scheer@rl-institut.de>
2025-06-17 18:24:11 +0200

    improve better term with passing name for term

--- lua/custom/plugins/betterTerm.lua
+++ lua/custom/plugins/betterTerm.lua
@@ -1,46 +1,48 @@
-function get_selection_or_line()
-  local mode = vim.fn.mode()
-  vim.print(mode)
-  if mode == 'v' or mode == 'x' or mode == 'V' then
-    local pos_start = vim.fn.getpos "'<"
-    local lines = vim.fn.getregion(vim.fn.getpos '.', vim.fn.getpos 'v')
-    return table.concat(lines, '\n')
-  -- elseif mode == 'V' then
-  --   local pos_end = vim.fn.getpos "'>"
-  --
-  --   local start_line = pos_start[2]
-  --   local end_line = pos_end[2]
-  --
-  --   if start_line > end_line then
-  --     start_line, end_line = end_line, start_line
-  --   end
-  --
-  --   local lines = vim.fn.getline(start_line, end_line)
-  --   return table.concat(lines, '\n')
-  else
-    vim.print 'single line'
-    -- No visual selection: fallback to current line
-    return vim.fn.getline '.'
-  end
-end
+-- use slime instead
+-- function get_selection_or_line()
+--   local mode = vim.fn.mode()
+--   vim.print(mode)
+--   if mode == 'v' or mode == 'x' or mode == 'V' then
+--     local pos_start = vim.fn.getpos "'<"
+--     local lines = vim.fn.getregion(vim.fn.getpos '.', vim.fn.getpos 'v')
+--     return table.concat(lines, '\n')
+--   -- elseif mode == 'V' then
+--   --   local pos_end = vim.fn.getpos "'>"
+--   --
+--   --   local start_line = pos_start[2]
+--   --   local end_line = pos_end[2]
+--   --
+--   --   if start_line > end_line then
+--   --     start_line, end_line = end_line, start_line
+--   --   end
+--   --
+--   --   local lines = vim.fn.getline(start_line, end_line)
+--   --   return table.concat(lines, '\n')
+--   else
+--     vim.print 'single line'
+--     -- No visual selection: fallback to current line
+--     return vim.fn.getline '.'
+--   end
+-- end
 return {
   'CRAG666/betterTerm.nvim',
   keys = {
     {
       mode = { 'n', 't' },
-      '<leader>t0',
+      '<leader>to',
       function()
-        require('betterTerm').open()
+        local ok, word = pcall(vim.fn.input, 'Create terminal: ')
+        if not ok then
+          return
+        end
+        if word == '' then
+          require('betterTerm').open()
+          return
+        end
+        print(word)
+        require('betterTerm').open(word)
       end,
       desc = 'Open BetterTerm 0',
-    },
-    {
-      mode = { 'n', 't' },
-      '<leader>t0',
-      function()
-        require('betterTerm').open(1)
-      end,
-      desc = 'Open BetterTerm 1',
     },
     {
       '<leader>tt',
@@ -48,26 +50,6 @@ return {
         require('betterTerm').select()
       end,
       desc = 'Select terminal',
-    },
-
-    {
-      mode = { 'n', 'v', 'x' },
-      '<leader>ts',
-      function()
-        local cmd = get_selection_or_line()
-        require('betterTerm').send(cmd, 0, 'interrupt')
-        -- Send an empty line to ensure final block is executed
-        -- Use feedkeys with correct termcodes to exit terminal insert mode
-        vim.cmd 'stopinsert' -- reliable alternative to <C-\><C-n>
-        -- Wait a bit to ensure the mode switches before running window commands
-        -- vim.defer_fn(function()
-        --   vim.print 'waiting'
-        -- end, 2000) -- delay in milliseconds
-        -- vim.print 'centering'
-        --
-        -- vim.cmd 'wincmd p'
-      end,
-      desc = 'Send to terminal 0',
     },
   },
   opts = {
