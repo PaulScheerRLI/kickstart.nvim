e72dfc076ca80f86fd926fb8a93486f7b1127f7b
PaulScheer <paul.scheer@rl-institut.de>
2025-07-04 17:21:45 +0200

    Fix snippets

--- init.lua
+++ init.lua
@@ -966,6 +966,9 @@ require('lazy').setup({
           {
             'rafamadriz/friendly-snippets',
             config = function()
+              require('luasnip.loaders.from_vscode').load {
+                exclude = { 'javascript' },
+              }
               require('luasnip.loaders.from_vscode').lazy_load {
                 paths = { vim.fn.stdpath 'config' .. '/snippets' },
               }
