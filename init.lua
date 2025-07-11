431699848aa8f5e8d7946521cde2fe1c123d6640
PaulScheerRLI <paul.scheer@rl-institut.de>
2025-07-11 23:29:06 +0200

    Add incremental selection via v and V

--- init.lua
+++ init.lua
@@ -1196,6 +1196,13 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          node_incremental = 'v',
+          node_decremental = 'V',
+        },
+      },
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
