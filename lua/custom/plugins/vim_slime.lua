4536dcbe3b99c45495cca27b4524b572732b3c0e
PaulScheerRLI <paul.scheer@rl-institut.de>
2025-07-27 11:46:16 +0200

    Fix slime for windows

--- lua/custom/plugins/vim_slime.lua
+++ lua/custom/plugins/vim_slime.lua
@@ -1,10 +1,10 @@
+-- THIS HAS TO HAPPEN BEFORE THE PLUGIN IS SETUP
+if vim.env.TMUX ~= nil then
+  vim.g.slime_target = 'tmux'
+else
+  vim.g.slime_target = 'neovim'
+end
 return {
   'jpalardy/vim-slime',
-  config = function()
-    if vim.env.TMUX ~= nil then
-      vim.g.slime_target = 'tmux'
-    else
-      vim.g.slime_target = 'neovim'
-    end
-  end,
+  config = function() end,
 }
